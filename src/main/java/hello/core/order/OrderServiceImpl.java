2a555489d24a8b9f2d58cf75cb6131dc3baf90d6
Raedeok-H <raedeokhwang@gmail.com>
2024-03-19 09:12:58 +0900

    인터페이스에만 의존하게 변경(구현체가 없음)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,13 +12,9 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    /** 클래스 의존관계를 분석해 보자. 추상(인터페이스) 뿐만 아니라 구체(구현) 클래스에도 의존하고 있다.
-        추상(인터페이스) 의존: DiscountPolicy
-        구체(구현) 클래스: FixDiscountPolicy , RateDiscountPolicy
-    **/
-    //지금 코드는 기능을 확장해서 변경하면, 클라이언트 코드에 영향을 준다! 따라서 OCP를 위반(DIP 위반)한다.
-    //인터페이스(추상)에만 의존하도록 설계를 변경
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy; //인터페이스에만 의존하게 변경.
+    // 구현체가 없음 -> 누군가가 클라이언트인 OrderServiceImpl 에 DiscountPolicy 의 구현 객체를 대신 생성하고 주입해줘야 함.
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
