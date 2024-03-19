a8077205855091f695a65aef6f2b8535a6ffbb92
Raedeok-H <raedeokhwang@gmail.com>
2024-03-19 09:33:47 +0900

    AppConfig 리팩터링(중복제거, 역할에 따른 구현이 보이도록)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -12,12 +14,21 @@ import hello.core.order.OrderServiceImpl;
  * 구현 객체를 생성하고,
  * 연결하는 책임을 가지는 별도의 설정 클래스
  */
+// AppConfig 에서, 중복을 제거하고 역할에 따른 구현이 보이도록 리팩터링.
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
