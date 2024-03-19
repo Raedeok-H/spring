6dcc566deb81dba79222f764ffbd0802877fc27b
Raedeok-H <raedeokhwang@gmail.com>
2024-03-19 16:13:17 +0900

    중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -14,6 +14,9 @@ package hello.core;
 // @Repository : 스프링 데이터 접근 계층에서 사용
 // @Configuration : 스프링 설정 정보에서 사용
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -23,5 +26,18 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 //보통 설정 정보를 컴포넌트 스캔 대상에서 제외하지는 않지만, 기존 예제 코드를 최대한 남기고 유지하기 위해서 이 방법을 선택했다.
 public class AutoAppConfig {
+    /*
+    * 1. 자동 빈 등록 vs 자동 빈 등록 -> (ConflictingBeanDefinitionException 예외 발생)
+    * 2. 수동 빈 등록 vs 자동 빈 등록 -> 수동 빈 등록이 우선권을 가진다.(수동 빈이 자동 빈을 오버라이딩 해버린다.)
+    *                                그래서서 최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꾸었다.
+    *
+    * ---스프링부트 에러메세지--- --->스프링 부트인 CoreApplication 을 실행해보면 오류를 볼 수 있다.
+    * Consider renaming one of the beans or enabling overriding by setting
+    * spring.main.allow-bean-definition-overriding=true
+    **/
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
