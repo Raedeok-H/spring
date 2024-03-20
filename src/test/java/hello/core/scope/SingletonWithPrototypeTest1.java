1bc2abcc473c5c78d7fda502c1e6fcdc1e2c631c
Raedeok-H <raedeokhwang@gmail.com>
2024-03-20 13:45:54 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시(JSR-330 자바 표준을 사용하는 방법)

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider; //import 주의
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,19 +44,20 @@ public class SingletonWithPrototypeTest1 {
     @Scope("singleton")
     static class ClientBean {
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider; //테스트니까 간단하게 필드주입으로 함
-        //ObjectProvider 사용, ObjectFactory 로 바꾸어도 동작함(ObjectProvider가 ObjectFactory를 상속받았기 때문)
-                            // ObjectFactory에는 getObject()메소드만 있고 ObjectProvider는 편의 기능(옵션,스트림처리) 을 더 제공
-                            // ObjectProvider는 "스프링에 의존"
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // 얘가 찾아줌 -> getObject()를 호출할 때, 스프링 컨테이너에서 프로토타입 빈을 찾아서 반환함
-                                                                             // 직접 찾는것이 아니라 찾아주는 기능만 제공
-                                                                             // 스프링이 제공하는 기능을 사용하지만, 기능이 단순하므로 단위테스트를 만들거나 mock 코드를 만들기는 훨씬쉬워진다.
-                                                                             // ObjectProvider 는 지금 딱 필요한 DL 정도의 기능만 제공한다.
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); //get() 메소드 사용
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
+        /*프로토타입 빈을 언제 사용할까? 매번 사용할 때 마다 의존관계 주입이 완료된 새로운 객체가 필요하면 사용하면 된다.
+        그런데 실무에서 웹 애플리케이션을 개발해보면, 싱글톤 빈으로 대부분의 문제를 해결할 수 있기 때문에 프로토타입 빈을 직접적으로 사용하는 일은 매우 드물다.
+
+        * ObjectProvider , JSR330 Provider 등은 프로토타입 뿐만 아니라 DL이 필요한 경우는 언제든지 사용할 수 있다.
+
+        * 참고: 스프링이 제공하는 메서드에 @Lookup 애노테이션을 사용하는 방법도 있지만, 이전 방법들로 충분하고, 고려해야할 내용도 많아서 생략하겠다
+        */
     }
 
     @Scope("prototype")
