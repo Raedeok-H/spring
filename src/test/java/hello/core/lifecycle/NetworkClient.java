3bb5c73312ee9dbe779570775c55e27cc2d90075
Raedeok-H <raedeokhwang@gmail.com>
2024-03-20 10:23:21 +0900

    @PostConstruct, @PreDestroy 애노테이션 지원(!!가장 권장됨!!) - 그냥 이거 쓰자

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,9 @@
 package hello.core.lifecycle;
+// javax나 jakarta로 시작하면 스프링이 아닌 다른 프레임워크를 사용해도 사용할 수 있음.
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient{
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -28,14 +28,29 @@ public class NetworkClient{
         System.out.println("close: " + url);
     }
 
-    public void init() throws Exception { // 빈 등록 초기화 메소드
+    @PostConstruct // 초기화 후 호출 애너테이션
+    public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    public void close() throws Exception { //소멸 메소드
+    @PreDestroy // 소멸전 호출 애너테이션
+    public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
     }
+
+    /*
+    * @PostConstruct, @PreDestroy 애노테이션 특징
+        최신 스프링에서 가장 권장하는 방법이다.
+        애노테이션 하나만 붙이면 되므로 매우 편리하다.
+        패키지를 잘 보면 javax.annotation.PostConstruct 이다. 스프링에 종속적인 기술이 아니라 JSR-250라는 자바 표준이다. 따라서 스프링이 아닌 다른 컨테이너에서도 동작한다.
+        컴포넌트 스캔과 잘 어울린다.
+
+        * 유일한 단점은 외부 라이브러리에는 적용하지 못한다는 것이다. 외부 라이브러리를 초기화, 종료 해야 하면 @Bean의 기능을 사용하자.
+
+    * !!!!!!!!!정리
+    * !!! @PostConstruct, @PreDestroy 애노테이션을 사용하자
+        코드를 고칠 수 없는 외부 라이브러리를 초기화, 종료해야 하면 @Bean 의 initMethod , destroyMethod 를 사용하자.*/
 }
