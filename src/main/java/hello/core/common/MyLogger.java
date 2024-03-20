ce23f371d03141dc2f23d7a8bf0d1a5ced5ef43a
Raedeok-H <raedeokhwang@gmail.com>
2024-03-20 15:25:16 +0900

    웹 스코프 예제 - proxy 사용

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -3,12 +3,16 @@ package hello.core.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
-@Scope(value = "request") //value 없이 써도 됨.
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) //값이 두개 이상일 경우 [키 = 밸류] 형태로 들어가야해서 키의 생략이 안 됨.
+                                                                    // 적용 대상이 클래스면 TARGET_CLASS 를 선택
+                                                                    // 적용 대상이 인터페이스면 INTERFACES 를 선택
+            //이렇게 하면 MyLogger의 가짜 프록시 클래스를 만들어두고 HTTP request와 상관 없이 가짜 프록시 클래스를 다른 빈에 미리 주입해 둘 수 있다
 public class MyLogger {
     private String uuid; //UUID를 사용해서 HTTP 요청을 구분
     private String requestURL;
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,7 +3,6 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,16 +11,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; //간단하게 ObjectProvider로 사용함
-    /*ObjectProvider 덕분에 ObjectProvider.getObject() 를 호출하는 시점까지 request scope 빈의 생성을 지연할 수 있다.
-    ObjectProvider.getObject() 를 호출하시는 시점에는 HTTP 요청이 진행중이므로 request scope 빈의 생성이 정상 처리된다.
-    ObjectProvider.getObject() 를 LogDemoController , LogDemoService 에서 각각 한번씩 따로 호출해도 같은 HTTP 요청이면 같은 스프링 빈이 반환된다!*/
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        System.out.println("myLogger = " + myLogger.getClass()); //myLogger = class hello.core.common.MyLogger$$SpringCGLIB$$0  => 가짜 프록시 클래스
+                                                                 //CGLIB(바이트코드 조작 라이브러리)를 확인할 수 있다. -> appConfig에서 @Configuration 애너테이션 설명과 같음
+                                                                 //ac.getBean("myLogger", MyLogger.class)처럼 조회해도
+                                                                 // 프록시 객체가 조회되고, 의존관계 주입도 프록시 객체가 주입됨
+
+                                                                 // ! 가짜 프록시 객체는 요청이 오면 그때 내부에서 진짜 빈을 요청하는 위임 로직이 들어있다.
+                                                                 //  가짜 프록시 객체는 실제 request scope와는 관계가 없다.
+                                                                 //  그냥 가짜이고, 내부에 단순한 위임 로직만 있고, 싱글톤 처럼 동작함.
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); //필요한 시점에 호출
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
