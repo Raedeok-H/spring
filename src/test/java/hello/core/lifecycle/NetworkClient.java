dadd3aa66a606ef11a34f176f394dfcdd49c71ce
Raedeok-H <raedeokhwang@gmail.com>
2024-03-20 10:07:29 +0900

    인터페이스(InitializingBean, DisposableBean) (초기화, 소멸)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,13 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -26,4 +27,25 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close: " + url);
     }
+
+    @Override //의존관계 주입이 끝나면 호출해줌. (초기화 콜백)
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override // 스프링 컨케이너 소멸후 호출 (소멸 콜백)
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+
+    /*
+    * 초기화, 소멸 인터페이스 단점
+        -이 인터페이스는 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스에 의존한다.
+        -초기화, 소멸 메서드의 이름을 변경할 수 없다.
+        -내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다.
+
+    * 참고: 인터페이스를 사용하는 초기화, 종료 방법은 스프링 초창기에 나온 방법들이고, 지금은 다음의 더 나은 방법들이 있어서 거의 사용하지 않는다.*/
 }
